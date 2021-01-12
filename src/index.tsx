fb31e0a94ad136e68c9ebf0bb62ecdee1572472f
Nicolas Chalet <nicolas.chalet@gmail.com>
2021-01-12 19:42:14 +0100

    chore(sw): Disabled cache for service worker

--- src/index.tsx
+++ src/index.tsx
@@ -37,7 +37,14 @@ if (process.env.NODE_ENV !== "production") {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onSuccess: (reg) => {
+    console.log("Success", reg);
+  },
+  onUpdate: (reg) => {
+    console.log("Update", reg);
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
