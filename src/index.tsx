cd317f22c26c296fe2edb68fb9776e15f2dca91c
Nicolas Chalet <nicolas.chalet@gmail.com>
2021-01-12 19:26:40 +0100

    chore(sw): Added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import { rffProps, store } from "./app/redux/store";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
 const render = (Comp: ElementType) => {
@@ -36,4 +37,9 @@ if (process.env.NODE_ENV !== "production") {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
